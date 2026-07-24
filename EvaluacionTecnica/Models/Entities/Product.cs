4586fcc5833c6ac8fc0542e5028b3dfd5c8e0b51
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-24 16:13:16 -0600

    Creando y definiendo propiedades de Product y agregando validaciones

---
+++ EvaluacionTecnica/Models/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EvaluacionTecnica.Models.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+
+        public string Codigo { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(150)]
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; }
+        [Range(0,double.MaxValue)]
+
+        public decimal Precio { get; set; }
+
+        public bool Estado { get; set; } = true;
+
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+        public Category Category { get; set; } = null!;
+    }
+}
