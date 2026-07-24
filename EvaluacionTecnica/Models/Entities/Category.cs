ae6b3cc8a4443c28ce5f4d9e0d48411d1b186460
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-24 16:11:59 -0600

    Agregando validaciones

--- EvaluacionTecnica/Models/Entities/Category.cs
+++ EvaluacionTecnica/Models/Entities/Category.cs
@@ -1,10 +1,14 @@
-﻿namespace EvaluacionTecnica.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EvaluacionTecnica.Models.Entities
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public required string Nombre { get; set; }
-        public bool Estado { get; set; }
+        public bool Estado { get; set; } = true;
 
     }
 }
