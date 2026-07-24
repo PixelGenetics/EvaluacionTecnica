c11ce01612f1fe5e5386c10f3ddcaf9b29b2388d
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-24 16:15:12 -0600

    Creando y definiendo propiedades de UpdateCategoryDto y agregando validaciones

---
+++ EvaluacionTecnica/Models/UpdateCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EvaluacionTecnica.Models
+{
+    public class UpdateCategoryDto
+    {
+        [StringLength(100)]
+        public required string Nombre { get; set; }
+        public bool Estado { get; set; }
+    }
+}
