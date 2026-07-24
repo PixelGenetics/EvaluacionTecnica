ac0149b5e88df9922478908735ceab8ffa728f3b
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-24 15:14:31 -0600

    Removiendo comentarios del codigo

---
+++ EvaluacionTecnica/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EvaluacionTecnica.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EvaluacionTecnica.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
