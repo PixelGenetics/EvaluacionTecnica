66c8e9b14ff6c85522f7fc79c8452984f98d64d1
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-24 15:13:45 -0600

    Agregando metodo para crear categoria

--- EvaluacionTecnica/Controllers/CategoryController.cs
+++ EvaluacionTecnica/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 ﻿using EvaluacionTecnica.Data;
+using EvaluacionTecnica.Models;
+using EvaluacionTecnica.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +21,33 @@ namespace EvaluacionTecnica.Controllers
         {
             var allCategories = await dbContext.Category.ToListAsync();
             return Ok(allCategories);
+        }
+
+        [HttpGet]
+        [Route("Nombre")]
+        public async Task<IActionResult> GetCategoryByName(string Nombre)
+        {
+            var category = await dbContext.Category.FirstOrDefaultAsync(category => category.Nombre == Nombre);
+
+            if (category == null)
+            {
+                return NotFound("El nombre que has ingresado no existe como categoria");
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(AddCategoryDto addCategoryDto)
+        {
+            var categoryEntity = new Category()
+            {
+                Nombre = addCategoryDto.Nombre,
+                Estado = addCategoryDto.Estado
+            };
+            await dbContext.Category.AddAsync(categoryEntity);
+            dbContext.SaveChanges();
+
+            return Ok(categoryEntity);
         }
     }
 }
