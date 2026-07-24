6cfe76247897e0231f9cf91ad5b3a3df99fbb277
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-24 14:32:27 -0600

    Visualizacion general de las categorias

---
+++ EvaluacionTecnica/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using EvaluacionTecnica.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EvaluacionTecnica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly AppDbContext dbContext;
+        public CategoryController(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var allCategories = await dbContext.Category.ToListAsync();
+            return Ok(allCategories);
+        }
+    }
+}
