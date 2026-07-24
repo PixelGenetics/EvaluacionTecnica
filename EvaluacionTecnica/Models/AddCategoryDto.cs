f80a1e9a78882e27ae2f082c94ed5dbb04c33a16
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-24 15:15:21 -0600

    Creando controlador para agregar categoria

---
+++ EvaluacionTecnica/Models/AddCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EvaluacionTecnica.Models
+{
+    public class AddCategoryDto
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public bool Estado { get; set; }
+    }
+}
