c02d228d652b02f149ca23fd6e16546c50e5a0b4
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-24 14:34:28 -0600

    Creacion de modelo de la tabla category

---
+++ EvaluacionTecnica/Models/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace EvaluacionTecnica.Models.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public bool Estado { get; set; }
+
+    }
+}
